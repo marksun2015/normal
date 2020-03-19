19d2a3de732be5c6c9fa20ee084b600b32c15a72
MarkSun <mark@weintek.com>
2020-03-19 19:02:26 +0800

    Remove useless code

--- factorymode/buzzer.cpp
+++ factorymode/buzzer.cpp
@@ -2,17 +2,6 @@
 #include "factory.h"
 #include "buzzer.h"
 
-#if 0
-class dev_buzzer: public Device
-{
-	public:
-		virtual void show()
-		{
-			printf("device is buzzer\n");
-		}
-};
-#endif
-
 void dev_buzzer:: show()
 {
 	printf("device is buzzer\n");
--- factorymode/speaker.cpp
+++ factorymode/speaker.cpp
@@ -2,22 +2,6 @@
 #include "factory.h"
 #include "speaker.h"
 
-#if 0
-class dev_speaker:public Device
-{
-	public:
-		int waveopen()
-		{
-			printf("speaker wave open\n");
-		}
-
-		virtual void show()
-		{
-			printf("device is speaker\n");
-			waveopen();
-		}
-};
-#endif
 int dev_speaker::waveopen()
 {
 	printf("speaker wave open\n");
