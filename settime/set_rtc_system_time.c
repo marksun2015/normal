67669b43b16c71b219e0719c1017d67f1fe40127
Mark Sun <mark@weintek.com>
2021-01-08 12:06:37 +0800

    Add sqlite by c

--- settime/set_rtc_system_time.c
+++ settime/set_rtc_system_time.c
@@ -1,52 +1,61 @@
-#include <linux/rtc.h> 
-#include <sys/ioctl.h> 
-#include <sys/time.h> 
-#include <sys/types.h> 
-#include <stdio.h> 
-#include <fcntl.h> 
-#include <unistd.h> 
-#include <errno.h> 
-#include <unistd.h>  
-#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/rtc.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <unistd.h>
 
-int main ()
-{
-    int ret;
-    struct tm info;
-    char buffer[80];
+int main() {
+  int ret;
+  // struct tm info;
+  struct tm info = {0};
+  char buffer[80];
 
-    time_t tval;
-    struct timeval tvval;
+  time_t tval;
+  struct timeval tvval;
 
-    info.tm_year = 2002 - 1900;
-    info.tm_mon = 7 - 1;
-    info.tm_mday = 4;
-    info.tm_hour = 0;
-    info.tm_min = 0;
-    info.tm_sec = 1;
-    info.tm_isdst = -1;
+  info.tm_year = 2017 - 1900;
+  info.tm_mon = 12 - 1;
+  info.tm_mday = 9;
+  info.tm_hour = 3;
+  info.tm_min = 1;
+  info.tm_sec = 30;
+  // info.tm_isdst = -1;
 
-    tval = mktime(&info);
-    tvval.tv_sec = (long)tval;
-    tvval.tv_usec = 0;
-    //set system time
-    if (settimeofday(&tvval, NULL) == 0) {
-        strftime(buffer, sizeof(buffer), "%c", &info);
-        printf("set time: %s \n",buffer);
+  printf("mktime \n");
+  tval = mktime(&info);
+  printf(">> %d \n", tval);
 
-        struct rtc_time time;
-        struct rtc_time new_time;
-        int fd = -1, ret;
+  tvval.tv_sec = (long)tval;
+  tvval.tv_usec = 0;
+  printf("settimeofday \n");
+  printf(">> %d \n", tvval.tv_sec);
+#if 1
+  // set system time
+  if (settimeofday(&tvval, 0) == 0) {
+    strftime(buffer, sizeof(buffer), "%c", &info);
+    printf("set time: %s \n", buffer);
 
-        memcpy(&time, &info, sizeof(struct rtc_time));
-        //set RTC
-        fd = open("/dev/rtc0", O_RDWR);
-        if (fd >= 0) {
-            ioctl(fd, RTC_SET_TIME, &time);
-            ret = ioctl(fd, RTC_RD_TIME, &new_time);
-        }
+    struct rtc_time time;
+    struct rtc_time new_time;
+    int fd = -1, ret;
+
+    memcpy(&time, &info, sizeof(struct rtc_time));
+    // set RTC
+    fd = open("/dev/rtc0", O_RDWR);
+    if (fd >= 0) {
+      ioctl(fd, RTC_SET_TIME, &time);
+      ret = ioctl(fd, RTC_RD_TIME, &new_time);
     }
+  } else {
+    printf("settimeofday failed \n");
+  }
+#endif
 
-    return 0;
+  return 0;
 }
