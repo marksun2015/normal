4520415ed9221a1b41b439851a06c3fd734fa4f4
Mark Sun <mark@weintek.com>
2022-03-05 15:10:36 +0800

    New install method

--- googletest_sample/build.sh
+++ googletest_sample/build.sh
@@ -1,1 +1,9 @@
+#$ git clone https://github.com/google/googletest
+#$ cd googletest
+#$ mkdir build
+#$ cd build
+#$ cmake -DCMAKE_CXX_FLAGS=-std=c++11 -Dgtest_build_samples=ON ..
+#$ make -j4
+#$ make install
+
 g++ sample1.cpp -o sample1 -Igtest/include -Lgtest/lib -lgtest -lpthread
